bda2669e8dc02471163d7b92cdab9ab21601aeb3
yapra <io.prapas@gmail.com>
2017-02-23 20:57:00 +0200

    dfass

--- hashcode_sample.py
+++ hashcode_sample.py
@@ -2,6 +2,7 @@ import os
 import math
 import random
 import itertools
+import sys
 
 
 class Pizza(object):
@@ -18,7 +19,25 @@ def read_file(filename):
     """Read the input file."""
     with open(filename, 'r') as fin:
     	line = fin.readline()
-    	rows, cols, min_ing, max_cells, = [int(num) for num in line.split()]
+    	v, e, r, c, x = [int(num) for num in line.split()]
+    	line = fin.readline()
+    	v_sizes = [int(num) for num in line.split()]
+    	end = []
+    	for i in range(e):
+    		l = []
+    		dl, cap = [int(num) for num in fin.readline().split()]
+    		l.append((-1,dl))
+    		for i in range(cap):
+    			c_id, lat = [int(num) for num in fin.readline().split()]
+    			l.append((c_id, lat))
+    		end.append(l)
+    	reqs = []
+    	for i in range(r):
+    		req, vid, endp = [int(num) for num in fin.readline().split()]
+    		reqs.append((req, vid, endp))
+    	return (v,e,r,c,x,v_sizes,end,reqs)
+
+
     	grid = []
     	for row in range(rows):
     		grid.append(f.readline)
@@ -39,23 +58,19 @@ def cut(pizza):
 	pass
 
 def main():
-    """Main function."""
+	#if len(sys.argv) < 3:
+	#	sys.exit('Syntax: %s <filename> <output>' % sys.argv[0])
 
-    if len(sys.argv) < 3:
-        sys.exit('Syntax: %s <filename> <output>' % sys.argv[0])
-
-    print 'Running on file: %s' % sys.argv[1]
-
+    #print 'Running on file: %s' % sys.argv[1]
     # read input file
-    pizza, min_ing, max_cells = read_file(sys.argv[1])
-
-    try:
-        cuts = cut(pizza, min_ing, max_cells)
-    except KeyboardInterrupt:
-        pass
+    (v,e,r,c,x,v_sizes,end,reqs) = read_file(sys.argv[1])
+    print v,e,r,c,x
+    print v_sizes
+    print end
+    print reqs
 
     # write output file
-    write_file(cuts, sys.argv[2])
+    #write_file(cuts, sys.argv[2])
 
 if __name__ == '__main__':
 	main()
