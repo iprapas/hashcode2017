2898d8ac3acad63d74804fb52375d8edc0657b78
yapra <io.prapas@gmail.com>
2017-02-24 17:45:24 +0200

    Make run.sh a bit more generic for future use

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cwd=`pwd`
+files=`ls ${cwd}/input`
 
-echo "Running on kittens"
-/bin/python src/hashcode2017.py input/kittens.in output/kittens.out
-echo "Running on me_at_the_zoo"
-/bin/python src/hashcode2017.py input/me_at_the_zoo.in output/me_at_the_zoo.out
-echo "Running on trending_today"
-/bin/python src/hashcode2017.py input/trending_today.in output/trending_today.out
-echo "Running on videos_worth_spreading"
-/bin/python src/hashcode2017.py input/videos_worth_spreading.in output/videos_worth_spreading.out
+for file in $files
+do
+    filename=$(basename "$file")
+    #extension="${filename##*.}"
+    filename="${filename%.*}"
+    echo "Running on ${filename}."
+    /usr/bin/python src/hashcode2017.py input/$file output/${filename}.out
+done
+
